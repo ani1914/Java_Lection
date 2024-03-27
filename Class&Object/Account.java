dd45b445a433f69a414c33d80c5dd17e97b82e3d
Anna-Maria <56381692+ani1914@users.noreply.github.com>
2024-03-27 10:32:31 +0200

    Update Account.java

--- Class&Object/Account.java
+++ Class&Object/Account.java
@@ -1,13 +1,4 @@
-package com.citb408;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Kostadinova
- */
+package com.citb408;
 public class Account {
 
     private int accountNumber;
